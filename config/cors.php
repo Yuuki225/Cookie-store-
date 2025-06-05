6b5c47f51d2c824d09dcdbcb2510241167840aff
Yuuki <kiyukrn@gmail.com>
2025-06-05 19:47:52 +0700

    first commit

---
+++ config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+   'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+   'allowed_methods' => ['*'],
+
+   'allowed_origins' => ['http://localhost:5173'], // sesuaikan dengan URL React-mu
+
+   'allowed_origins_patterns' => [],
+
+   'allowed_headers' => ['*'],
+
+   'exposed_headers' => [],
+
+   'max_age' => 0,
+
+   'supports_credentials' => true, // <--- WAJIB!
+
+
+];
